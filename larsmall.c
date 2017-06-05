78d2ef996038b786f9bcf9a01f0a05253e2d2a79
manibharathsundararajan <bakka.sase@gmail.com>
2017-06-05 18:03:03 +0530

    Update larsmall.c

--- larsmall.c
+++ larsmall.c
@@ -1,23 +1,27 @@
+
+
 #include<stdio.h>
-void main()
-{
-int i,j,a[10],n,t;
-scanf("%d",n);
-for(i=0;i<n;i++)
-{
-scanf("%d",a[i]);
-}
-for(i=0;i<n;i++)
-{
-for(j=i+1;j<n;j++)
-{
-if(a[i]>a[j])
-{
-t=a[i];
-a[i]=a[j];
-a[j]=t;
-}
-}
-}
-printf("%d \n %d",a[0],a[n-1]);
-}
+	void main()
+	{
+	int i,j,a[10],n,t;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	for(j=i+1;j<n;j++)
+	{
+	if(a[i]>a[j])
+	{
+	t=a[i];
+	a[i]=a[j];
+	a[j]=t;
+	}
+	}
+	}
+	printf("%d \n %d",a[0],a[n-1]);
+	}
+
+
