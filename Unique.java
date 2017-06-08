3d958edfbd65fc9db0f24d394bd20f31eeeebaf6
manibharathsundararajan <bakka.sase@gmail.com>
2017-06-08 18:40:53 +0530

    Create Unique.java

---
+++ Unique.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Large {
+
+    public static void main(String[] args)
+    {
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    int flag=0;
+    int[] a=new int[n];
+    for(int i=0;i<n;i++)
+    {
+        a[i]=s.nextInt();
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(a[i]==a[j] && i!=j)
+                flag=1;
+        }
+        if(flag==0)
+    {
+        System.out.println(a[i]);
+    } flag=0;
+    }
+   
+    
+    
+}
+}
