56b4f3087fa94b3ede280843fed5e14392d12494
manibharathsundararajan <bakka.sase@gmail.com>
2017-06-09 17:38:13 +0530

    Create back.java

---
+++ back.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class back
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        StringBuffer a=new StringBuffer();
+        a.append(s.nextLine());
+        System.out.println(a.reverse());
+    }
+}
